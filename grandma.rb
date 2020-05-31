3d2a0919c9c7db7b4d1c8123016ed860fc731ca6
Mario Terrazas <mtrrzs@hotmail.com>
2020-05-31 23:57:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,3 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
+def speak_to_grandma(phrase)
+    if phrase != phrase.upcase 
+        return "HUH?! SPEAK UP, SONNY!" 
+    elsif phrase == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    
+    elsif phrase == phrase.upcase 
+        return "NO, NOT SINCE 1938!" 
+    end   
+end 
